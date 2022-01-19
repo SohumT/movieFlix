f7b7ad70b727ccd217bc99bfd11198b9b0e4cdc8
SohumT <ssthadan@purdue.edu>
2022-01-19 17:15:19 -0500

    Initial Commit

---
+++ movieFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieFlix
+//
+//  Created by Sohum Thadani on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
