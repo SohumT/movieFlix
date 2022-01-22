5389fd651e146842c88da50d761e57d30bd4fba8
SohumT <ssthadan@purdue.edu>
2022-01-22 16:21:43 -0500

    First Commit

---
+++ MovieCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  movieFlix
+//
+//  Created by Sohum Thadani on 1/22/22.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
